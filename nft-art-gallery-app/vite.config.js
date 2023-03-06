b1b83705b26a6e68f1e0f194ce985692912d3777
maudenahutchinson <maudenahutchinson@gmail.com>
2023-03-06 14:01:24 +0800

    cleaning code

--- nft-art-gallery-app/vite.config.js
+++ nft-art-gallery-app/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"http://MaudHutch86.github.io/NFT-Art-gallery-Hackaton",
   plugins: [react()],
 })
